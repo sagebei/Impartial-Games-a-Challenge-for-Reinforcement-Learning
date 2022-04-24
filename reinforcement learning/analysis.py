fix the the sign of the Q value

reinforcement learning/analysis.py
@@ -8,7 +8,7 @@ set_seed(30)
 
 # board size: [1, 3, 5, 7, 9]
 heaps = [1, 3, 3, 0, 0]
-num_simulation = 10000
+num_simulation = 1000
 
 state = []
 for i, counters in enumerate(heaps):
@@ -28,7 +28,8 @@ game = NimEnv(num_piles=len(heaps))
 model = Nim_Model(action_size=game.action_size,
                   hidden_size=128,
                   num_layers=1)
-model.load_state_dict(torch.load('./models/5_1'))
+# model.load_state_dict(torch.load('./models/5_piles_latest_model'))
+model.load_state_dict(torch.load('./models/5_200'))
 args = {'num_simulations': num_simulation,  
         'alpha': 0.35,
         'c_puct': 3}
@@ -41,23 +42,26 @@ visit_count_distribution = visit_counts / sum(visit_counts)
 for i, (action, node) in enumerate(root.children.items()):
     child_state = []
     sum_counter = 0
-    for counter in list(np.array(node.state, dtype=np.int8)):
-        if counter == -1:
-            child_state.append(sum_counter)
-            sum_counter = 0
-        else:
-            sum_counter += counter
-    child_state.append(sum_counter)
     
-    print(f'Child: {child_state}', end='   ')
-    print(f'P:{node.prior}', end="  ")
-    _, value = model.predict(node.state)
-    print(f'V:{value}', end=" ")
-    print(f'N: {node.visit_count}({visit_count_distribution[i]}%)', end='   ')
-    print(f'Q value:{node.value()}')
+    # if the node has been visited
+    if node.state is not None: 
+        for counter in list(np.array(node.state, dtype=np.int8)):
+            if counter == -1:
+                child_state.append(sum_counter)
+                sum_counter = 0
+            else:
+                sum_counter += counter
+        child_state.append(sum_counter)
+        
+        print(f'Child: {child_state}', end='   ')
+        print(f'P:{node.prior}', end="  ")
+        _, value = model.predict(node.state)
+        print(f'V:{value}', end=" ")
+        print(f'N: {node.visit_count}({visit_count_distribution[i]}%)', end='   ')
+        print(f'Q value:{-node.value()}')
 
 _, value = model.predict(root.state)
 print(f'root:{heaps} ', end='')
 print(f'V:{value}', end=" ")
-print(f'Q value:{root.value()}')
+print(f'WL:{root.value()}')
 